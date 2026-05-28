58d51c4e372f329b289c069f2aa9f401efbdb299
Alexander Holas <alexander.holas@mailbox.org>
2026-05-28 17:25:44 +0200

    Evrards collapse test

--- src/pgen/evrards_collapse.cpp
+++ src/pgen/evrards_collapse.cpp
@@ -24,10 +24,9 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
   auto &u = pmb->meshblock_data.Get()->Get("cons").data;
   auto &coords = pmb->coords;
 
-  auto &phi = pmb->meshblock_data.Get()->Get("gravity.phi").data;
-  auto &rhs = pmb->meshblock_data.Get()->Get("gravity.rhs").data;
+  auto pmb_data = pmb->meshblock_data.Get();
+  auto &phi = pmb_data->Get("gravity.phi").data;
   auto gravity_g = pin->GetOrAddReal("gravity", "gravity_constant", GRAVITY_G);
-  const Real four_pi_g = 4.0 * M_PI * gravity_g;
 
   pmb->par_for(
       "ProblemGenerator Evrard", kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
@@ -48,8 +47,27 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
         u(IM3, k, j, i) = 0.0;
         u(IEN, k, j, i) = u_therm * rho;
 
-        rhs(0, k, j, i) = four_pi_g * rho;
         phi(0, k, j, i) = -gravity_g * 1.0 / r;
       });
+
+  if (pmb_data->HasVariable("gravity.rhs")) {
+    auto &rhs = pmb_data->Get("gravity.rhs").data;
+    const Real four_pi_g = 4.0 * M_PI * gravity_g;
+    pmb->par_for(
+        "ProblemGenerator Evrard RHS", kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
+        KOKKOS_LAMBDA(const int k, const int j, const int i) {
+          Real x = coords.Xc<1>(i);
+          Real y = coords.Xc<2>(j);
+          Real z = coords.Xc<3>(k);
+          Real r = std::sqrt(SQR(x) + SQR(y) + SQR(z));
+          Real rho = 1e-5;
+
+          if (r < radius) {
+            rho = mass / (2.0 * M_PI * SQR(radius) * r);
+          }
+
+          rhs(0, k, j, i) = four_pi_g * rho;
+        });
+  }
 }
 } // namespace ec
